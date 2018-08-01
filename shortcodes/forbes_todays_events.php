67879510b5a39b3b43e6f31adcb852c520a3d5d0
Benjamin Kalish <bkalish@gmail.com>
2018-08-01 12:13:40 -0400

    Update forbes_todays_events.php

--- shortcodes/forbes_todays_events.php
+++ shortcodes/forbes_todays_events.php
@@ -6,7 +6,7 @@
  * 
  * @wp-hook add_shortcode forbes_todays_events
  */
-function forbes_todays_events() {
+function forbes_todays_events_shortcode_handler() {
   ob_start();
   ?>
 <style scoped> #api_today_cid228_iid1448 .s-lc-ea-date, #api_today_cid228_iid1448 .s-lc-ea-h3 { display:none; }</style>
